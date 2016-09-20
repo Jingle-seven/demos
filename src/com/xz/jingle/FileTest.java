aca2bb0b7096a63d47dbf9d5fb50269600799b01
Jingle_seven <xiong_jinhua@foxmail.com>
2016-09-20 17:20:17 +0800

    commit

--- src/com/xz/jingle/FileTest.java
+++ src/com/xz/jingle/FileTest.java
@@ -1,13 +1,17 @@
 package com.xz.jingle;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Jinhua on 2016/9/2.
  */
 public class FileTest {
-	public static void main(String[] args) {
-		File file = new File("G:/temp//0");
-		System.out.println(file.list());
+	public static void main(String[] args) throws IOException {
+		String path = "G:/temp/0/4.txt";
+		File dir = new File(path.substring(0,path.lastIndexOf("/")));
+		File file = new File(path);
+		dir.mkdirs();
+		file.createNewFile();
 	}
 }
