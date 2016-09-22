6b1902798465bc60e231f8b2c6d8b88dfaceaf61
Jingle_seven <xiong_jinhua@foxmail.com>
2016-09-22 15:14:03 +0800

    加入maven

--- src/com/xz/jingle/FileTest.java
+++ src/com/xz/jingle/FileTest.java
@@ -8,10 +8,15 @@ import java.io.IOException;
  */
 public class FileTest {
 	public static void main(String[] args) throws IOException {
-		String path = "G:/temp/0/4.txt";
-		File dir = new File(path.substring(0,path.lastIndexOf("/")));
-		File file = new File(path);
-		dir.mkdirs();
-		file.createNewFile();
+//		String path = "G:/temp/0/4.txt";
+//		File dir = new File(path.substring(0,path.lastIndexOf("/")));
+//		File file = new File(path);
+//		dir.mkdirs();
+//		file.createNewFile();
+		System.out.println("123456".substring(3));
+		System.out.print(System.getProperty("file.encoding"));
+		String str = new File("conf/config.prop").getAbsolutePath();
+//		new File("conf/config.prop").mkdirs();
+		System.out.println(str);
 	}
 }
