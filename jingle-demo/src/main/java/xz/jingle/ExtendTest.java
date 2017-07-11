7178befdc24d847323c1c1043e2969193b8e9bad
xjh <xiong_jinhua@foxmail.com>
2017-07-11 09:51:50 +0800

    change extend test

--- jingle-demo/src/main/java/xz/jingle/ExtendTest.java
+++ jingle-demo/src/main/java/xz/jingle/ExtendTest.java
@@ -2,6 +2,7 @@ package xz.jingle;
 
 /**
  * Created by Jinhua on 2016/12/2.
+ *
  */
 //子类变量会将父类变量隐藏,但仍可以通过super访问
 public class ExtendTest {
@@ -12,14 +13,17 @@ public class ExtendTest {
 		
 		System.out.println(an.name);
 		System.out.println(dog.name);
-		System.out.println(an.kind);
 		
-		an.name="Cat";
-		System.out.println(an.name);
-		dog.setName("");
-		System.out.println(dog.toString());
+		//无论引用是什么,只要是子类对象,调用的方法都是子类方法
+		an.setName("橘猫");
+		dog.setName("拉布拉多");
+		System.out.println("animal'info: "+an);
+		System.out.println("dog's info: "+dog);
 		
-		dog.show();
+		//而静态属性和方法,似乎完全和继承无关
+		System.out.println(Animal.getKind());
+		System.out.println(Dog.getKind());
+		
 	}
 	
 }
@@ -29,9 +33,20 @@ class Animal{
 	public void run(){
 		System.out.println("Animal running!!!!");
 	}
+	
+	public static void setKind(String kind) {
+		Animal.kind = kind;
+	}
+	public static String getKind() {
+		return kind;
+	}
+	public Animal setName(String name) {
+		this.name = name;
+		return this;
+	}
+	
 	@Override
 	public String toString() {
-		
 		return "Animal{" +
 				"name='" + name + '\'' +
 				'}';
@@ -43,22 +58,32 @@ class Dog extends Animal{
 		this.name=name;
 	}
 	static String kind = "move faster";
+	
+	public static void setKind(String kind) {
+		Dog.kind = kind;
+	}
+	
+	public static String getKind() {
+		return kind;
+	}
+	
 	String name = "dog";
 	private Integer age;
-	public void show(){
-		System.out.println(super.toString());
-		System.out.println(this.toString());
-	}
-	public String setName(String name){
-		String temp = super.name;
-		super.name = "name changed to "+name;
+	public Dog setName(String name){
 		this.name = name;
-		return temp;
+		return this;
 	}
 	public String getName(){
 		return name;
 	}
-	public String toString(){
-		return "Dog's name is "+ name;
+	
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("Dog{");
+		sb.append("name='").append(name).append('\'');
+		sb.append(", age=").append(age);
+		sb.append('}');
+		sb.append(" & ").append(super.toString());
+		return sb.toString();
 	}
 }
