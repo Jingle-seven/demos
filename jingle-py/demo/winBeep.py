7d79165374efbc95610b629006a7e22116c354f9
xjh <xiong_jinhua@foxmail.com>
2017-09-01 11:31:27 +0800

    add: spring-boot test

---
+++ jingle-py/demo/winBeep.py
@@ -0,0 +1,7 @@
+import winsound
+
+winsound.Beep(5000, 1000)
+# winsound.PlaySound("*", winsound.SND_ALIAS)
+# winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+winsound.MessageBeep()
+print("end")
