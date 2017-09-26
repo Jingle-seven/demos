30b9cacae484648047606de86005ca25b3f4cdf3
xjh <xiong_jinhua@foxmail.com>
2017-09-26 17:39:54 +0800

    add: spring-boot exec jar

--- jingle-demo/src/main/java/xz/jingle/ReflectTest.java
+++ jingle-demo/src/main/java/xz/jingle/ReflectTest.java
@@ -1,5 +1,7 @@
 package xz.jingle;
 
+import org.junit.Test;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -9,7 +11,8 @@ import java.lang.reflect.Modifier;
  * Created by Jinhua on 2017/2/24.
  */
 public class ReflectTest {
-	public static void main(String[] args) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+	@Test
+	public void test() throws IllegalAccessException, InstantiationException, InvocationTargetException {
 		Class<?> dogClazz = Dog.class;
 		Dog dog = (Dog)dogClazz.newInstance();
 		Class<?> dogClazz2 = dog.getClass();
@@ -19,20 +22,46 @@ public class ReflectTest {
 		Field[] fields = dogClazz.getDeclaredFields();
 		
 		for (Method method : methods) {
-			System.out.println(method);
+			System.out.println("方法: "+method);
 //			System.out.println(method.invoke(dog));
-			System.out.println(method.getReturnType());
+			System.out.println("方法返回值: "+method.getReturnType());
 		}
+		//类成员的一些属性
 		for (Field field : fields) {
 			System.out.println("=================="+field);
 			field.setAccessible(true);
-			System.out.println(field.getModifiers());
+			System.out.println("修饰符: "+field.getModifiers());
 			Modifier.isStatic(field.getModifiers());
-			System.out.println(field.getType());
-			System.out.println(field.getName());
+			System.out.println("类型: "+field.getType());
+			System.out.println("名字: "+field.getName());
 			field.set(dog2,field.get(dog));
-			System.out.println(dog2.getName());
+			System.out.println("设置后的值: "+dog2.getName());
 			
 		}
+	}
+	
+	@Test//final变量修改成功?
+	public void testModifyFinalField() throws NoSuchFieldException, IllegalAccessException {
+		SwimmingDog dog = new SwimmingDog();
+		Field ageField = dog.getClass().getDeclaredField("age");
+		System.out.println("修饰符: "+ ageField.getModifiers());
+		ageField.setAccessible(true);
+		ageField.set(dog,1);
+		System.out.println("age: "+dog.getAge());
+	}
+}
+	
+
+class SwimmingDog extends Dog{
+	private final int age;
+	SwimmingDog(){
+		age = 0;
+	}
+	SwimmingDog(int age){
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return age;
 	}
 }
