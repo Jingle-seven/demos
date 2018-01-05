f6fee6113ca9d4c3f043e15c7e1a9fff417416bb
xjh <xiong_jinhua@foxmail.com>
2018-01-05 17:03:41 +0800

    Merge remote-tracking branch 'origin/dev' into dev
    # Conflicts:
    #	.gitignore
    #	jingle-demo/src/py/demo/gui.py
    #	jingle-demo/src/py/demo/sendAjax.py
    #	src/pydemo/demo/Gui.py
    #	src/pydemo/demo/SendAjax.py
    #	src/pydemo/demo/gui.py
    #	src/pydemo/demo/sendAjax.py

--- jingle-demo/src/main/java/xz/jingle/StringTest.java
+++ jingle-demo/src/main/java/xz/jingle/StringTest.java
@@ -5,14 +5,30 @@ package xz.jingle;
  */
 public class StringTest {
 	public static void main(String[] args) {
-		String s1 = "12,13,15,5847,87,8787,4787,8,58,71,648,46";
-		String s2 = "12";
-		String[] sa1 = s2.split(",");
-		System.out.println(sa1.length);
-		
-		String str1 = "insert into `cloudins_field` (`id`, `name`, `company`, `remark`) " +
-				"values (?, ?, ?, ?)','20170222150406','141',FALSE,FALSE)";
-		;
-		System.out.println(str1.replace("`","").replace("\'",""));
+        testSpilit();
+        testReplaceMany();
+        System.out.println(reverse("End of the world"));
+    }
+
+    private static void testSpilit() {
+        String s1 = "12,13,15,5847,87,8787,4787,8,58,71,648,46";
+        String s2 = "12";
+        System.out.println(s2.split(",").length);
+        System.out.println(s1.split(",").length);
+    }
+
+    private static void testReplaceMany() {
+        String str1 = "insert into `cloudins_field` (`id`, `name`, `company`, `remark`) " +
+                "values (?, ?, ?, ?)','20170222150406','141',FALSE,FALSE)";
+        System.out.println(str1.replace("`","").replace("\'",""));
+    }
+
+    public static String reverse(String originStr){
+	    int originLength = originStr.length();
+	    char[] resChars = new char[originLength];
+        for (int i = 0; i < originLength; i++) {
+            resChars[i] = originStr.charAt(originLength-1-i);
+        }
+        return new String(resChars);
 	}
 }
