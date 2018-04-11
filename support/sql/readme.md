f3c0852d2cdc74b6ac2fe8f28614658d960f5e04
jinhua <xiong_jinhua@foxmail.com>
2018-04-11 17:34:49 +0800

    change divide world.sql into three part

--- support/sql/readme.md
+++ support/sql/readme.md
@@ -1,2 +1,4 @@
+book.sql 对应jingle-py的一些文件以及jingle-server的dao  
 butler.sql 中的表对应jingle-server的model和idao  
-world.sql 对应jingle-py的一些文件以及jingle-server的dao
+world.sql 是mysql安装自带的数据  
+test.sql 是散乱的sql语句,t_user表在jingle-jpa和jingle-ebean中有用到  
---
+++ support/sql/test.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS `test` /*!40100 COLLATE 'utf8_general_ci' */;
+USE `test`;
+
+CREATE TABLE `t_user` (
+  `id` bigint(20) NOT NULL DEFAULT '0',
+  `name` varchar(10) NOT NULL DEFAULT '',
+  `account` varchar(50) NOT NULL DEFAULT '',
+  `password` varchar(50) NOT NULL DEFAULT '',
+  `authority` tinyint(3) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `account` (`account`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+
+
+INSERT INTO `t_user` VALUES (0,'tom','tom','tom',1),(1495852626527,'nancysss','nancy','nancy',0),(1496215868322,'jim','jim','jim',0);
