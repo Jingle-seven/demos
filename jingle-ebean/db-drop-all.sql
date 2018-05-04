832df27baf5b764ef2f5d91113b43dcc09e16ad3
jinhua <xiong_jinhua@foxmail.com>
2018-05-04 08:53:38 +0800

    add test related self

--- jingle-ebean/db-drop-all.sql
+++ jingle-ebean/db-drop-all.sql
@@ -1,10 +1,10 @@
-alter table tb_user drop foreign key fk_tb_user_user_tag_id;
-drop index ix_tb_user_user_tag_id on tb_user;
-
 alter table tb_user drop foreign key fk_tb_user_dept_id;
 drop index ix_tb_user_dept_id on tb_user;
 
 alter table tb_user drop foreign key fk_tb_user_address_post_number;
+
+alter table tb_tag drop foreign key fk_tb_tag_user_tag_id;
+drop index ix_tb_tag_user_tag_id on tb_tag;
 
 drop table if exists tb_address;
 
--- jingle-ebean/src/main/java/xz/model/Dept.java
+++ jingle-ebean/src/main/java/xz/model/Dept.java
@@ -43,7 +43,7 @@ public class Dept {
         return "Dept{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", users=" + l +
+                ", children=" + l +
                 '}';
     }
 }
